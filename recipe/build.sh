e952b7dd811ebadfa7246341ede2782600ba7894
Max Rakitin <mrakitin@bnl.gov>
2023-06-15 22:56:39 +0000

    Debussy feedstock config on top of `hkl-feedstock`

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,8 +8,4 @@ test -d m4 || mkdir m4
 export ACLOCAL_PATH="$PREFIX/share/aclocal"
 aclocal --print-ac-dir
 
-autoreconf -ivf
-
-./configure --disable-static --disable-gui --enable-introspection=yes --disable-hkl-doc --prefix=$PREFIX
-make -j ${CPU_COUNT}
-make install
+echo -e "$PREFIX\ny\ny\ny\n" | ./install_debussy_v2.2
