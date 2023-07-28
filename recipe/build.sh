b747fd0db5aa2622c60d41066e8e2cde72227778
Maksim Rakitin <mrakitin@bnl.gov>
2023-07-28 11:04:18 -0400

    Add more diagnostics

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,3 +10,8 @@ aclocal --print-ac-dir
 
 cd UNIX/DEBUSSY_v2.2
 echo -e "$PREFIX\ny\ny\ny\n" | ./install_debussy_v2.2
+
+echo -e "\nCHECKING EPDL97.inc\n"
+cat $SRC_DIR/UNIX/DEBUSSY_v2.2/MAKING_gfortran/EPDL97.inc
+echo -e "\nEND CHECKING EPDL97.inc\n"
+
